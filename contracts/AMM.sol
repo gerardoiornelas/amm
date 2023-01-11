f0611e5823f5add9bc66645e6029e4f320ea1767
Gerardo Ornelas <gerardo@ornelastechnologies.com>
2023-01-11 08:13:43 -0600

    add liquidity tokens w/test

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -2,5 +2,25 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "./Token.sol";
 
-contract AMM {}
+contract AMM {
+    Token public token1;
+    Token public token2;
+
+    constructor(Token _token1, Token _token2) {
+        token1 = _token1;
+        token2 = _token2;
+    }
+
+    function addLiquidity(
+        uint256 _token1Amount,
+        uint256 _token2Amount
+    ) external {
+        // Deposit tokens
+        token1.transferFrom(msg.sender, address(this), _token1Amount);
+        token2.transferFrom(msg.sender, address(this), _token2Amount);
+        // Issue shares
+        // Manage pool
+    }
+}
--- test/AMM.test.js
+++ test/AMM.test.js
@@ -8,23 +8,51 @@ const tokens = (n) => {
 const ether = tokens;
 
 describe("AMM", () => {
-  let accounts, deployer, token1, token2, amm;
+  let accounts, deployer, liquidityProvider, token1, token2, amm;
 
   beforeEach(async () => {
     accounts = await ethers.getSigners();
     deployer = accounts[0];
+    liquidityProvider = accounts[1];
 
     const Token = await ethers.getContractFactory("Token");
     token1 = await Token.deploy("Ornelas.tech", "OTECH", "1000000");
     token2 = await Token.deploy("USD Token", "USD", "1000000");
 
+    // send tokens to liquidity provider
+    let transaction = await token1
+      .connect(deployer)
+      .transfer(liquidityProvider.address, tokens(100000));
+    await transaction.wait();
+
+    transaction = await token2
+      .connect(deployer)
+      .transfer(liquidityProvider.address, tokens(100000));
+    await transaction.wait();
+
     const AMM = await ethers.getContractFactory("AMM");
-    amm = await AMM.deploy();
+    amm = await AMM.deploy(token1.address, token2.address);
   });
 
   describe("Deployment", () => {
     it("has an address", async () => {
       expect(amm.address).to.not.equal(0x0);
+    });
+
+    it("tracks token1 address", async () => {
+      expect(await amm.token1()).to.equal(token1.address);
+    });
+
+    it("tracks token2 address", async () => {
+      expect(await amm.token2()).to.equal(token2.address);
+    });
+  });
+
+  describe("Swapping Tokens", () => {
+    let amount;
+    it("facilates swaps", async () => {
+      // deployer approves 100k tokens
+      amount = tokens(100000);
     });
   });
 });
